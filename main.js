fa03a7f186dc6e51511f065d608c50c12b57938f
CauaSerpa <91634425+CauaSerpa@users.noreply.github.com>
2021-10-19 20:39:11 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+const showMenu = (toggleId, navId) =>{
+	const toggle = document.getElementById(toggleId),
+	nav = document.getElementById(navId)
+
+	if (toggle && nav){
+		toggle.addEventListener('click', ()=>{
+			nav.classList.toggle('show')
+		})
+	}
+}
+showMenu('nav-toggle','nav-menu')
+
+const navLink = document.querySelectorAll('.link')
+
+function linkAction(){
+	// Active link
+	navLink.forEach(n => n.classList.remove('active'))
+	this.classList.add('active')
+
+	const navMenu = document.getElementById('nav-menu')
+	navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//scroll reveal animation
+const sr = ScrollReveal({
+	origin: 'top',
+	distance: '80px',
+	duretion: 2000,
+	reset: true
+})
+//scroll home
+sr.reveal('.image_5',{interval: 200})
+sr.reveal('.image_4',{interval: 200})
+sr.reveal('.image_1',{})
+sr.reveal('.home__title',{delay: 200})
+sr.reveal('.buttom',{delay: 400})
